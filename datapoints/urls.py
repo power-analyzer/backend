a734baacc88c35ef176521f01561869079c228cf
Ryan Rabello <ryan.s.rabello@gmail.com>
2018-05-20 21:30:14 -0700

    Added more endpoints

--- datapoints/urls.py
+++ datapoints/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('<str:mac>/batch/', measurements.batch_upload, name="batch_upload"),
     path('buildings/<int:id>', buildings.get_building, name="get_buildings"),
     path('buildings', buildings.get_buildings, name="get_buildings"),
+    path('devices/<int:id>/circuits', devices.get_device_circuits, name="get_device_circuits"),
     path('devices/<int:id>', devices.get_device, name="get_device"),
     path('devices', devices.get_devices, name="get_devices"),
     path('circuits/<int:id>/<str:end_date>/<str:start_date>', circuits.get_usage),
--- datapoints/views/devices.py
+++ datapoints/views/devices.py
@@ -3,12 +3,16 @@ from django.shortcuts import get_object_or_404
 from django.core import serializers
 from django.forms.models import model_to_dict
 
-from datapoints.models import Device
+from datapoints.models import Device, Circuit
 
 
 def get_devices(request):
     devices = Device.objects.all()
     return HttpResponse(serializers.serialize('json', devices))
+
+def get_device_circuits(request, id):
+    circuits = Circuit.objects.filter(device=id);
+    return HttpResponse(serializers.serialize('json', circuits))
 
 def get_device(request, id):
     device = get_object_or_404(Device, id=id)
