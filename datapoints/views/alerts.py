b52952a3672dec7e61005effcb51a3ee657b1b17
Ryan Rabello <ryan.s.rabello@gmail.com>
2018-06-04 01:08:24 -0700

    Finished Email Notifications

--- datapoints/views/alerts.py
+++ datapoints/views/alerts.py
@@ -1,26 +1,25 @@
 import json
 import logging
 
-from django.shortcuts import get_object_or_404
-from django.http import HttpResponse, JsonResponse
 from django.core import serializers
+from django.shortcuts import get_object_or_404
+from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse, JsonResponse
 
 from datapoints.models import Alert, Circuit
 
-
+@require_http_methods(["GET", "POST"])
 def get_all_alerts_or_add_alert(request):
-    logger = logging.getLogger(__name__)
     logger.info(request.method)
     if request.method == "POST":
         body_unicode = request.body.decode('utf-8')
         body = json.loads(body_unicode)
-        logger.info(body)
         # Create new alert
         alert = Alert()
         # Get the circuit for the
-        circuit = get_object_or_404(Circuit, id=getattr(body, "circuit"))
+        circuit = get_object_or_404(Circuit, id=body["circuit"])
         alert.circuit = circuit
-        del body.circuit
+        del body["circuit"]
 
         for key, value in body.items():
             setattr(alert, key, value)
@@ -31,5 +30,18 @@ def get_all_alerts_or_add_alert(request):
         alerts = Alert.objects.all()
         return HttpResponse(serializers.serialize('json', alerts))
 
+@require_http_methods(["POST"])
 def edit_alert(request, id):
-    pass
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    # Create new alert
+    alert = get_object_or_404(Alert, id=id)
+    # Get the circuit for the
+    circuit = get_object_or_404(Circuit, id=body["circuit"])
+    alert.circuit = circuit
+    del body["circuit"]
+
+    for key, value in body.items():
+        setattr(alert, key, value)
+    alert.save()
+    return JsonResponse({"status": "success"})
